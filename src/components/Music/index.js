2ddf790688514561a26d98606f1ee1cf99f824e8
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-02-13 11:59:14 -0800

    Add data fetching from ec2 server/s3

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -1,38 +1,54 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
-const ARTISTS = [
-  'Red Hot Chili Peppers',
-  'Stone Temple Pilots',
-  'Led Zeppelin'
-];
-
-const ALBUMS = [
-  'album 1',
-  'album 2'
-];
-
-const SONGS = [
-  'song 1',
-  'song 2'
-];
-
 class Artists extends Component {
   state = {
+    artists: null,
+    albums: null,
+    songs: null,
     selectedArtist: null,
     selectedAlbum: null,
     selectedSong: null
   };
 
+  componentDidMount() {
+    fetch('http://ec2-3-82-153-6.compute-1.amazonaws.com:8081/songs')
+    .then(response => response.json())
+    .then(responseJson => {
+      let artists = [];
+      let albums = [];
+      let songs = [];
+      responseJson.body.records.forEach(record => {
+        let key = record.Key;
+        key = key.replace(/\/$/, '');
+        if (key.split('/').length === 3) {
+          songs.push(key);
+        } else if (key.split('/').length === 2) {
+          albums.push(key);
+        } else if (key.split('/').length === 1) {
+          artists.push(key);
+        }
+      });
+      this.setState({
+        artists,
+        albums,
+        songs
+      });
+    })
+  }
+
   onClickArtist(selectedArtist) {
     this.setState({
-      selectedArtist
+      selectedArtist,
+      selectedAlbum: null,
+      selectedSong: null
     });
   }
 
   onClickAlbum(selectedAlbum) {
     this.setState({
-      selectedAlbum
+      selectedAlbum,
+      selectedSong: null
     });
   }
 
@@ -44,31 +60,43 @@ class Artists extends Component {
 
   render() {
     const {
+      artists,
+      albums,
+      songs,
       selectedArtist,
       selectedAlbum,
       selectedSong
-    } = this.state;
+    } = this.state;    
 
     return (
       <div>
-        <ArtistsList
-          artists={ARTISTS}
-          selectedArtist={selectedArtist}
-          onClickArtist={this.onClickArtist.bind(this)}
-        />
-        <AlbumsList
-          albums={ALBUMS}
-          selectedArtist={selectedArtist}
-          selectedAlbum={selectedAlbum}
-          onClickAlbum={this.onClickAlbum.bind(this)}
-        />
-        <SongsList
-          songs={SONGS}
-          selectedArtist={selectedArtist}
-          selectedAlbum={selectedAlbum}
-          selectedSong={selectedSong}
-          onClickSong={this.onClickSong.bind(this)}
-        />
+        {artists
+          ? <ArtistsList
+              artists={artists}
+              selectedArtist={selectedArtist}
+              onClickArtist={this.onClickArtist.bind(this)}
+            />
+          : null
+        }
+        {albums
+          ? <AlbumsList
+              albums={albums}
+              selectedArtist={selectedArtist}
+              selectedAlbum={selectedAlbum}
+              onClickAlbum={this.onClickAlbum.bind(this)}
+            />
+          : null
+        }
+        {songs
+          ? <SongsList
+              songs={songs}
+              selectedArtist={selectedArtist}
+              selectedAlbum={selectedAlbum}
+              selectedSong={selectedSong}
+              onClickSong={this.onClickSong.bind(this)}
+            />
+          : null
+        }
       </div>
     );
   }
@@ -93,14 +121,16 @@ const AlbumsList = (props) => {
   if (props.selectedArtist) {
     return (
       <div>
-        {props.albums.map(album => 
-          <Button
-            onClick={() => props.onClickAlbum(album)}
-            key={album}
-          >
-            {album}
-          </Button>
-        )}
+        {props.albums.filter(album => album.split('/')[0] === props.selectedArtist)
+          .map(album => 
+            <Button
+              onClick={() => props.onClickAlbum(album)}
+              key={album}
+            >
+              {album.split('/')[1]}
+            </Button>
+          )
+        }
       </div>
     );
   } else {
@@ -112,14 +142,16 @@ const SongsList = (props) => {
   if (props.selectedAlbum) {
     return (
       <div>
-        {props.songs.map(song => 
-          <Button
-            onClick={() => props.onClickSong(song)}
-            key={song}
-          >
-            {song}
-          </Button>
-        )}
+        {props.songs.filter(song => song.split('/')[0] + '/' + song.split('/')[1] === props.selectedAlbum)
+          .map(song => 
+            <Button
+              onClick={() => props.onClickSong(song)}
+              key={song}
+            >
+              {song.split('/')[2]}
+            </Button>
+          )
+        }
       </div>
     );
   } else {
